5224dbdc02d8a44c1f3d5f86503660042f2b839b
WALID SASSI <sassi.walid@outlook.com>
2024-11-01 19:33:09 +0100

    handle temporary issue with Testing on github action CICD

--- Tests/SparkDITests/SparkDITests.swift
+++ Tests/SparkDITests/SparkDITests.swift
@@ -5,6 +5,8 @@ import XCTest
 #endif
 @testable import SparkDI
 
+#if canImport(Testing)
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+#endif
